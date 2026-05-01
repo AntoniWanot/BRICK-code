572445c3f821e4b7f3d0b9a3199149a0fafb83a2
bored_ami <azmozgame@gmail.com>
2026-05-01 17:49:04 +0200

    new layout for pcb ^^

--- esp32_code/include/config.h
+++ esp32_code/include/config.h
@@ -3,13 +3,13 @@
 
 #define WIFI_SSID "ESP32_WebServer"
 #define WIFI_PASSWORD "12345678"
-#define READY_SIGNAL 22 // signal from esp32 to teensy that it is ready to receive manifest, 13 teensy
-#define MOTOR1_PLUS_PIN 2 // motor 1 JOG+ 36 teensy
-#define MOTOR1_MINUS_PIN 4// motor 1 JOG- 37 teensy
-#define MOTOR2_PLUS_PIN 23 // motor 2 JOG+ 38 teensy
-#define MOTOR2_MINUS_PIN 18// motor 2 JOG-  39 teensy
-#define MOTOR3_PLUS_PIN 19 // motor 3 JOG+ 40 teensy
-#define MOTOR3_MINUS_PIN 21// motor 3 JOG- 41 teensy
+#define READY_SIGNAL 33 // signal from esp32 to teensy that it is ready to receive manifest, 13 teensy
+#define MOTOR1_PLUS_PIN 13 // motor 1 JOG+ 36 teensy
+#define MOTOR1_MINUS_PIN 12// motor 1 JOG- 37 teensy
+#define MOTOR2_PLUS_PIN 14 // motor 2 JOG+ 38 teensy
+#define MOTOR2_MINUS_PIN 27// motor 2 JOG-  39 teensy
+#define MOTOR3_PLUS_PIN 26 // motor 3 JOG+ 40 teensy
+#define MOTOR3_MINUS_PIN 25// motor 3 JOG- 41 teensy
 #define RXD2 16
 #define TXD2 17
 #endif
